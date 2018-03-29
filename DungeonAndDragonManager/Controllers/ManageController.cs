802dc6b806975a30912ed696123369d3f41312fd
Erik Buter <e.t.buter@gmail.com>
2018-03-29 16:43:59 +0200

    More work on schema and import file

--- DungeonAndDragonManager/Controllers/ManageController.cs
+++ DungeonAndDragonManager/Controllers/ManageController.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.Options;
 using DungeonAndDragonManager.Models;
 using DungeonAndDragonManager.Models.ManageViewModels;
 using DungeonAndDragonManager.Services;
+// ReSharper disable ConsiderUsingConfigureAwait
 
 namespace DungeonAndDragonManager.Controllers
 {
